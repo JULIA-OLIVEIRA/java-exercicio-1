63a618f8d3a3b5ec0deb1aa41ae1f4fdee6ba06c
Matuk <mateusmatuk@fai-mg.br>
2020-02-19 20:45:34 -0300

    segundo commit

--- src/com/fai/Main.java
+++ src/com/fai/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String [] args) {
 		
-		System.out.println("Insira quatro valores inteiros");
+		System.out.println("Insira cinco valores inteiros");
 		
 		}
 
